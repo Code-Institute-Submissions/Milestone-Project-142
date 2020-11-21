68e677bc9c812369795ae5e44185c76222a972c3
ryanjbm <rjbyrne22@gmail.com>
2020-11-21 12:07:45 +0000

    Added google maps api with custom markers and pinned locations, changed about and nav bar bakground colors and uploaded images

---
+++ assets/js/javascript.js
@@ -0,0 +1,36 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 40.426775, lng: -3.703790 },
+    zoom: 12,
+  });
+
+ var locations = [
+      ['<b>Santiago Bernabeu</b>' + "<br>Real Madrid's Stadium <br><img src='assets/images/bernabeu.png' alt='' width='250px' height='150px'>", 40.4530, -3.6883, 4],
+      ['<b>Vicente Calderon</b>' + "<br>Atletico Madrid's Stadium"+ "<br><img src='assets/images/calderon.png' alt='' width='300px' height='150px'>", 40.4017, -3.7206, 5],
+      ['<b>Royal Palace</b>' + '<br>Home to the Spanish Royal Family'+ "<br><img src='assets/images/madridmain.png' alt='' width='300px' height='150px'>", 40.4180, -3.7143, 3],
+      ['<b>Gran Via</b>' + '<br>Main Shoping high street in Madrid'+ "<br><img src='assets/images/granvia.png' alt='' width='300px' height='200px'>", 40.4200, -3.7021, 2],
+      ["<b>Plaza Mayor</b>" + "<br>Most famous plaza in all of spain<br> located in the center of Madrid with many restaurants" + "<br><img src='assets/images/plazamayor.png' alt='' width='300px' height='150px'>", 40.4155, -3.7074, 1]
+    ];
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+    const image = src='assets/images/orange30.png';
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map,
+         icon: image
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+}
