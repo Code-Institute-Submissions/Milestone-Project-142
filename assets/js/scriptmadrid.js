b1e3582cb218fe25ca34659a2bcceb214941bbdd
ryanjbm <rjbyrne22@gmail.com>
2020-11-26 10:55:41 +0000

    Added sliding effect on content when scrolling

--- assets/js/scriptmadrid.js
+++ assets/js/scriptmadrid.js
@@ -57,3 +57,28 @@ $(function(){
         }
     });
 });
+
+
+
+const appearOptions = {
+    threshold:1,
+    rootMargin: "0px 0px -50px 0px"
+};
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOnScroll = new IntersectionObserver
+(function(entries, appearOnScroll){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting) {
+            return;
+        } else{
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+},
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
