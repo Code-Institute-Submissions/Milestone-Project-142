c48b599af55579b710f51b0f313fd8835f43d858
ryanjbm <rjbyrne22@gmail.com>
2020-11-28 00:23:18 +0000

    Fixed read more button

--- assets/js/scriptmadrid.js
+++ assets/js/scriptmadrid.js
@@ -106,21 +106,20 @@ function backToTop(){
 
 
 
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
-  
+function readMore(city) {
+    let dots = document.querySelector(`.card[card-num="${city}"] .dots`);
+    let moreText = document.querySelector(`.card[card-num="${city}"] .more`); 
+    let btnText = document.querySelector(`.card[card-num="${city}"] .myBtn`);
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more";
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less";
-    moreText.style.display = "inline";
-  }
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less"; 
+        moreText.style.display = "inline";
+    }
 }
 
 
